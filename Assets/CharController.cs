c9a2a42eb2316f32fddf2c41f2522fd3dc00020d
Mikołaj Nawrocki <m.nawrocki793@gmail.com>
2020-08-02 14:50:24 +0200

    3. Part Three
    - system animacji

--- Assets/CharController.cs
+++ Assets/CharController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class CharController : MonoBehaviour
 {
@@ -16,20 +17,40 @@ public class CharController : MonoBehaviour
     public float camYSpeed;
     public float speedRotation = 1.0f;
     public float speed = 1.0f;
+    public float jumpForce = 1f;
+    public float speedRun = 4f;
+    public float speedWalk = 2f;
 
     public Rigidbody rigidbody;
-    public Transform CameraH1;
-    public Transform CameraH2;
+    public LayerMask groundLayer;
+    public CapsuleCollider col;
+    public Transform cameraH1;
+    public Transform cameraH2;
+    public Transform cameraH1Zero;
+    public Transform camerah2Zero;
+    Animator anim;
 
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = speedRun;
+            anim.SetBool("run", true);
+        }
+        else
+        {
+            speed = speedWalk;
+            anim.SetBool("walk", true);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     private void FixedUpdate()
@@ -45,19 +66,10 @@ public class CharController : MonoBehaviour
         }
         else
         {
+            Cursor.visible = true;
             HeroMoveNormal();
+            if (Input.GetAxis("Horizontal") != 0 || (Input.GetAxis("Vertical") != 0)) CameraBack();
         }
-    }
-
-    private void MoveCamera()
-    {
-        Cursor.visible = false;
-        cameraHolder1Move.y = Input.GetAxis("Mouse X");
-        CameraH1.Rotate(cameraHolder1Move, camXSpeed);
-
-        cameraHolder2Move.x = Input.GetAxis("Mouse Y") * -1f;
-        CameraH2.Rotate(cameraHolder2Move, camYSpeed);
-
     }
 
     private void MovePPM()
@@ -85,12 +97,18 @@ public class CharController : MonoBehaviour
         mouseMove.y = Input.GetAxis("Mouse X");
         Quaternion mouseRot = Quaternion.Euler(mouseMove * speed);
         rigidbody.MoveRotation(rigidbody.rotation * mouseRot);
+
+        cameraHolder2Move.x = Input.GetAxis("Mouse Y") * -1f;
+        cameraH2.Rotate(cameraHolder2Move, camYSpeed);
     }
 
     void HeroMoveNormal()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+
+        anim.SetFloat("v", v);
+        anim.SetFloat("h", h);
 
         if (v > 0) move = transform.forward;
         else if (v < 0) move = transform.forward * -1.0f;
@@ -104,5 +122,24 @@ public class CharController : MonoBehaviour
         }
 
         rigidbody.MovePosition(transform.localPosition + move * speed * Time.deltaTime);
+    }
+    private void MoveCamera()
+    {
+        Cursor.visible = false;
+        cameraHolder1Move.y = Input.GetAxis("Mouse X");
+        cameraH1.Rotate(cameraHolder1Move, camXSpeed);
+
+        cameraHolder2Move.x = Input.GetAxis("Mouse Y") * -1f;
+        cameraH2.Rotate(cameraHolder2Move, camYSpeed);
+
+    }
+    void CameraBack()
+    {
+        cameraH1.rotation = Quaternion.Lerp(cameraH1.rotation, cameraH1Zero.rotation, Time.deltaTime * speed);
+        cameraH2.rotation = Quaternion.Lerp(cameraH2.rotation, cameraH1Zero.rotation, Time.deltaTime * speed);
+    }
+    bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayer);
     }
 }
